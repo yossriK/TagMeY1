9688afd7b2c57fbb5893872e3cd5c281442eddd6
yossriK <yossri.khalil@gmail.com>
2020-03-11 11:51:05 -0600

    Add project files.

---
+++ WebApplication4/Controllers/accountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class accountController : Controller
+    {
+
+        private SignInManager<AppUser> SignInMgr { get; set; }
+
+        private UserManager<AppUser> UserMgr { get; set; }
+       public accountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            UserMgr = userManager;
+            SignInMgr = signInManager;
+        }
+
+        public async Task<IActionResult> Login()
+        {
+
+            var result = await SignInMgr.PasswordSignInAsync("TestUser", "test123", false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Result = "result is: " + result.ToString();
+            }
+
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> Register( string uname, string psw, string email)
+        {
+
+            try
+            {
+                ViewBag.Message = "User already exists";
+
+                AppUser user = await UserMgr.FindByNameAsync(uname);
+                if (user == null)
+                {
+                    user = new AppUser();
+                    user.UserName = uname;
+                    user.Email = email;
+                    user.FirstName = "John";
+                    user.LastName = "Doe'";
+
+                    IdentityResult result = await UserMgr.CreateAsync(user, psw);
+                    ViewBag.Message = "UserWas created";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+
+
+
+            return View();
+        }
+
+        public  IActionResult RegisterPage()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/IdentityAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class IdentityAppContext :IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public IdentityAppContext(DbContextOptions<IdentityAppContext> options): base(options)
+        {
+
+        }
+    }
+}
